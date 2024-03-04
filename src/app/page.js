bd4c90adcbf5b28a8a60c3e2a143bee10e4d4014
Tamer Fouad <tamer.fbhb.wd@gmail.com>
2024-03-04 12:20:46 +0200

    add slider for projects in home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-
+'use client'
 import Image from "next/image";
 import styles from "./page.module.scss";
 import { CiClock1 } from "react-icons/ci";
@@ -7,10 +7,37 @@ import PulseDott from "@/component/pulseDott/PulseDott";
 import download from '../../public/download.png'
 import { MdOutlineKeyboardArrowRight, MdOutlineKeyboardArrowLeft } from "react-icons/md";
 import Link from "next/link";
-
+import { useEffect, useState } from "react";
+import axios from 'axios';
 
 
 export default function Home() {
+
+  // to git data
+  const [post, setPost] = useState([])
+  const [error, setError] = useState(null)
+
+  async function getAllPosts() {
+    try {
+      const { data } = await axios.get("https://dummyjson.com/products");
+      setPost(data.products.slice(0,8))
+    } catch (error) {
+        setError(error.message)
+    }
+}
+  useEffect(() => {
+    getAllPosts();
+  }, []);
+
+  
+  //to move cards with arrow
+  const [index, setIndex] = useState('left');
+  const moveRight = () => {
+    setIndex('left');
+  };
+  const moveLeft = () => {
+    setIndex('right');
+  };
   return (
     <main className={styles.main}>
       <div className={styles.about}>
@@ -100,13 +127,41 @@ export default function Home() {
             </Link>
 
             <div className={styles.arrows}>
-              <div className={styles.leftArrow}>
+              <div className={`${styles.leftArrow} ${index === 'left' ? styles.noActivee : styles.activee}`}
+              onClick={moveRight}>
                 <MdOutlineKeyboardArrowLeft className={styles.iconArrow} />
               </div>
-              <div className={styles.rightArrow}>
+              <div className={`${styles.rightArrow} ${index === 'right' ? styles.noActivee : styles.activee}`}
+              onClick={moveLeft}>
                 <MdOutlineKeyboardArrowRight className={styles.iconArrow} />
               </div>
             </div>
+
+          </div>
+        </div>
+
+        <div className={styles.tapBody}>
+          <div className={`${styles.cards} ${index === 'right' ? styles.moveCards : styles.backMove}`}>
+            {error == null ? (post.map((item) => {
+              return (
+                <Link href={`/projects/${item.id}`}>
+                  <div className={styles.card}>
+                    <div className={styles.imgCard}>
+                      <Image
+                        src={item.thumbnail}
+                        alt="Picture of the author"
+                        fill
+                        className={styles.img}
+                      />
+                    </div>
+                    <div className={styles.bodyCard}>
+                      <h5>brand : {item.brand}  </h5>
+                      <h5>price : {item.price} $ </h5>
+                    </div>
+                  </div>
+                </Link>
+              )
+            })) : (<h1>Request failed with status code 404 </h1>)}
 
           </div>
         </div>
